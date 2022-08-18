8fbd86ea7ea048d0956ed2b819f06bb0ff953a2d
jed <oange6214@gmail.com>
2022-08-19 00:32:22 +0800

    加入專案檔案。

---
+++ WPF_UserInterfaceDesign/MainWindow.xaml.cs
@@ -0,0 +1,55 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+
+namespace WPF_UserInterfaceDesign
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void textEmail_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            txtEmail.Focus();
+        }
+
+        private void txtEmail_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            textEmail.Visibility = !string.IsNullOrEmpty(txtEmail.Text) && txtEmail.Text.Length > 0 ? Visibility.Collapsed : Visibility.Visible;
+        }
+
+        private void textPassword_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            txtPassword.Focus();
+        }
+
+        private void txtPassword_PasswordChanged(object sender, RoutedEventArgs e)
+        {
+            textPassword.Visibility = !string.IsNullOrEmpty(txtPassword.Password) && txtPassword.Password.Length > 0 ? Visibility.Collapsed : Visibility.Visible;
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            if (!string.IsNullOrEmpty(txtEmail.Text) && !string.IsNullOrEmpty(txtPassword.Password))
+            {
+                MessageBox.Show("Successfully Signed In");
+            }
+        }
+
+        private void Border_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.ChangedButton == MouseButton.Left)
+            {
+                this.DragMove();
+            }
+        }
+
+        private void Image_MouseUp(object sender, MouseButtonEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+    }
+}
