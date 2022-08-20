0881077cb41fb136ded6e2d61c0e5dfa1253f575
jed <oange6214@gmail.com>
2022-08-21 00:34:52 +0800

    Add Responsive Instagram App Project

---
+++ ResponsiveInstagramApp/MainWindow.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System.Windows;
+
+namespace ResponsiveInstagramApp
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void textSearch_MouseDown(object sender, System.Windows.Input.MouseButtonEventArgs e)
+        {
+            txtSearch.Focus();
+        }
+
+        private void txtSearch_TextChanged(object sender, System.Windows.Controls.TextChangedEventArgs e)
+        {
+            textSearch.Visibility = !string.IsNullOrEmpty(txtSearch.Text) && txtSearch.Text.Length > 0
+                ? Visibility.Collapsed
+                : Visibility.Visible;
+        }
+    }
+}
---
+++ ResponsiveInstagramApp/UserControls/Profile.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+
+namespace ResponsiveInstagramApp.UserControls
+{
+    public partial class Profile : UserControl
+    {
+        public Profile()
+        {
+            InitializeComponent();
+        }
+
+
+
+        public int PaddingNum
+        {
+            get { return (int)GetValue(PaddingNumProperty); }
+            set { SetValue(PaddingNumProperty, value); }
+        }
+
+        public static readonly DependencyProperty PaddingNumProperty =
+            DependencyProperty.Register(nameof(PaddingNum), typeof(int), typeof(Profile));
+
+        public int Size
+        {
+            get { return (int)GetValue(SizeProperty); }
+            set { SetValue(SizeProperty, value); }
+        }
+
+        public static readonly DependencyProperty SizeProperty =
+            DependencyProperty.Register(nameof(Size), typeof(int), typeof(Profile));
+
+
+
+        public ImageSource Source
+        {
+            get { return (ImageSource)GetValue(SourceProperty); }
+            set { SetValue(SourceProperty, value); }
+        }
+
+        public static readonly DependencyProperty SourceProperty =
+            DependencyProperty.Register(nameof(Source), typeof(ImageSource), typeof(Profile));
+    }
+}
