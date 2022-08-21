8ee6b5fd7fb50c6a30e45d7e2ac15897f7e42d4a
jed <oange6214@gmail.com>
2022-08-22 00:04:50 +0800

    Add Flat Shopping Cart Project

---
+++ FlatShoppingCart/MainWindow.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System.Windows;
+using System.Windows.Input;
+
+namespace FlatShoppingCart
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void StackPanel_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.ChangedButton == MouseButton.Left)
+            {
+                this.DragMove();
+            }
+        }
+    }
+}
---
+++ FlatShoppingCart/UserControls/Product.xaml.cs
@@ -0,0 +1,70 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+
+namespace FlatShoppingCart.UserControls
+{
+    public partial class Product : UserControl
+    {
+        public Product()
+        {
+            InitializeComponent();
+        }
+
+
+        public ImageSource Source
+        {
+            get { return (ImageSource)GetValue(SourceProperty); }
+            set { SetValue(SourceProperty, value); }
+        }
+
+        public static readonly DependencyProperty SourceProperty =
+            DependencyProperty.Register(nameof(Source), typeof(ImageSource), typeof(Product));
+
+        public string Title
+        {
+            get { return (string)GetValue(TitleProperty); }
+            set { SetValue(TitleProperty, value); }
+        }
+
+        public static readonly DependencyProperty TitleProperty =
+            DependencyProperty.Register(nameof(Title), typeof(string), typeof(Product));
+
+        public string Ref
+        {
+            get { return (string)GetValue(RefProperty); }
+            set { SetValue(RefProperty, value); }
+        }
+
+        public static readonly DependencyProperty RefProperty =
+            DependencyProperty.Register(nameof(Ref), typeof(string), typeof(Product));
+
+        public string Color
+        {
+            get { return (string)GetValue(ColorProperty); }
+            set { SetValue(ColorProperty, value); }
+        }
+
+        public static readonly DependencyProperty ColorProperty =
+            DependencyProperty.Register(nameof(Color), typeof(string), typeof(Product));
+
+        public string Count
+        {
+            get { return (string)GetValue(CountProperty); }
+            set { SetValue(CountProperty, value); }
+        }
+
+        public static readonly DependencyProperty CountProperty =
+            DependencyProperty.Register(nameof(Count), typeof(string), typeof(Product));
+
+        public string Price
+        {
+            get { return (string)GetValue(PriceProperty); }
+            set { SetValue(PriceProperty, value); }
+        }
+
+        public static readonly DependencyProperty PriceProperty =
+            DependencyProperty.Register(nameof(Price), typeof(string), typeof(Product));
+
+    }
+}
