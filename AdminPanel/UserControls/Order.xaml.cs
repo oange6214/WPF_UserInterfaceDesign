904ea2b75ad8ba658659e469e6da8226c0fae03b
jed <oange6214@gmail.com>
2022-08-26 07:49:28 +0800

    Add AdminPanel Project

---
+++ AdminPanel/UserControls/Order.xaml.cs
@@ -0,0 +1,45 @@
+﻿using FontAwesome.Sharp;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace AdminPanel.UserControls
+{
+    public partial class Order : UserControl
+    {
+        public Order()
+        {
+            InitializeComponent();
+        }
+
+
+        public string Title
+        {
+            get { return (string)GetValue(TitleProperty); }
+            set { SetValue(TitleProperty, value); }
+        }
+
+        public static readonly DependencyProperty TitleProperty =
+            DependencyProperty.Register(nameof(Title), typeof(string), typeof(Order));
+
+
+        public string Desc
+        {
+            get { return (string)GetValue(DescProperty); }
+            set { SetValue(DescProperty, value); }
+        }
+
+        public static readonly DependencyProperty DescProperty =
+            DependencyProperty.Register(nameof(Desc), typeof(string), typeof(Order));
+
+
+        public IconChar Icon
+        {
+            get { return (IconChar)GetValue(IconProperty); }
+            set { SetValue(IconProperty, value); }
+        }
+
+        public static readonly DependencyProperty IconProperty =
+            DependencyProperty.Register(nameof(Icon), typeof(IconChar), typeof(Order));
+
+    }
+}
