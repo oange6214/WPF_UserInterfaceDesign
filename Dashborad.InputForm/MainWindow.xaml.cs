d0ba1b94358bc0fe90200a909e5cf5cc10d84bd0
jed <oange6214@gmail.com>
2022-08-30 07:14:41 +0800

    Add Dashborad InputForm Project

---
+++ Dashborad.InputForm/MainWindow.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System.Windows;
+using System.Windows.Input;
+
+namespace Dashborad.InputForm
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Border_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if(e.ChangedButton == MouseButton.Left)
+            {
+                this.DragMove();
+            }
+        }
+
+        private bool IsMaximized = false;
+        private void Border_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.ClickCount == 2)
+            {
+                if (IsMaximized)
+                {
+                    this.WindowState = WindowState.Normal;
+                    this.Width = 1250;
+                    this.Height = 830;
+
+                    IsMaximized = false;
+                }
+                else
+                {
+                    this.WindowState = WindowState.Maximized;
+
+                    IsMaximized = true;
+                }
+            }
+        }
+    }
+}
---
+++ Dashborad.InputForm/UserControls/MyTextBox.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace Dashborad.InputForm.UserControls
+{
+    public partial class MyTextBox : UserControl
+    {
+        public MyTextBox()
+        {
+            InitializeComponent();
+        }
+
+        public string Hint
+        {
+            get { return (string)GetValue(HintProperty); }
+            set { SetValue(HintProperty, value); }
+        }
+
+        public static readonly DependencyProperty HintProperty =
+            DependencyProperty.Register(nameof(Hint), typeof(string), typeof(MyTextBox));
+
+        public string Caption
+        {
+            get { return (string)GetValue(CaptionProperty); }
+            set { SetValue(CaptionProperty, value); }
+        }
+
+        public static readonly DependencyProperty CaptionProperty =
+            DependencyProperty.Register(nameof(Caption), typeof(string), typeof(MyTextBox));
+
+    }
+}
