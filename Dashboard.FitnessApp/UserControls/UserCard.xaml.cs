08dea918311b41a470477e8ba237f32504cceee5
jed <oange6214@gmail.com>
2022-09-04 08:35:31 +0800

    Dashboard Fitness App Add Right Section

---
+++ Dashboard.FitnessApp/UserControls/UserCard.xaml.cs
@@ -0,0 +1,61 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+
+namespace Dashboard.FitnessApp.UserControls
+{
+    public partial class UserCard : UserControl
+    {
+        public UserCard()
+        {
+            InitializeComponent();
+        }
+
+
+        public string FullName
+        {
+            get { return (string)GetValue(FullNameProperty); }
+            set { SetValue(FullNameProperty, value); }
+        }
+
+        public static readonly DependencyProperty FullNameProperty =
+            DependencyProperty.Register(nameof(FullName), typeof(string), typeof(UserCard));
+
+        public string Title
+        {
+            get { return (string)GetValue(TitleProperty); }
+            set { SetValue(TitleProperty, value); }
+        }
+
+        public static readonly DependencyProperty TitleProperty =
+            DependencyProperty.Register(nameof(Title), typeof(string), typeof(UserCard));
+
+        public string Time
+        {
+            get { return (string)GetValue(TimeProperty); }
+            set { SetValue(TimeProperty, value); }
+        }
+
+        public static readonly DependencyProperty TimeProperty =
+            DependencyProperty.Register(nameof(Time), typeof(string), typeof(UserCard));
+
+        public ImageSource Image
+        {
+            get { return (ImageSource)GetValue(ImageProperty); }
+            set { SetValue(ImageProperty, value); }
+        }
+
+        public static readonly DependencyProperty ImageProperty =
+            DependencyProperty.Register(nameof(Image), typeof(ImageSource), typeof(UserCard));
+
+        public bool IsActive
+        {
+            get { return (bool)GetValue(IsActiveProperty); }
+            set { SetValue(IsActiveProperty, value); }
+        }
+
+        public static readonly DependencyProperty IsActiveProperty =
+            DependencyProperty.Register(nameof(IsActive), typeof(bool), typeof(UserCard));
+
+    }
+}
