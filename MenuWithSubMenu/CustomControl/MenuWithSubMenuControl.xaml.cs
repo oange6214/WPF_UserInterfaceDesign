25d667e8831e507af818c0ea826c47145b656712
jed <oange6214@gmail.com>
2023-02-18 00:35:28 +0800

    Add MenuWithSubMenu

---
+++ MenuWithSubMenu/CustomControl/MenuWithSubMenuControl.xaml.cs
@@ -0,0 +1,37 @@
+﻿using MenuWithSubMenu.ViewModel;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace MenuWithSubMenu.CustomControl
+{
+    public partial class MenuWithSubMenuControl : UserControl
+    {
+        public MenuWithSubMenuControl()
+        {
+            InitializeComponent();
+            
+            // Binding our ViewModel with the datacontext to read the Menu & SubMenuItemsData
+            DataContext = new SideMenuViewModel();
+        }
+
+        public Thickness SubMenuPadding
+        {
+            get { return (Thickness)GetValue(SubMenuPaddingProperty); }
+            set { SetValue(SubMenuPaddingProperty, value); }
+        }
+
+        public static readonly DependencyProperty SubMenuPaddingProperty =
+            DependencyProperty.Register(nameof(SubMenuPadding), typeof(Thickness), typeof(MenuWithSubMenuControl));
+
+
+        public bool HasIcon
+        {
+            get { return (bool)GetValue(HasIconProperty); }
+            set { SetValue(HasIconProperty, value); }
+        }
+
+        public static readonly DependencyProperty HasIconProperty =
+            DependencyProperty.Register(nameof(HasIcon), typeof(bool), typeof(MenuWithSubMenuControl));
+
+    }
+}
---
+++ MenuWithSubMenu/MainWindow.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Windows;
+
+namespace MenuWithSubMenu
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            MainWindowFrame.Navigate(new Uri(string.Format("Pages/Dashboard.xaml"), UriKind.RelativeOrAbsolute));
+        }
+    }
+}
