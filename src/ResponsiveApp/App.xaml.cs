e1d0053f19e80e28506dd7e8573bd97440204339
Pohan <oange6214@gmail.com>
2024-06-26 08:57:43 +0800

    Add ResponsiveApp

---
+++ src/ResponsiveApp/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace ResponsiveApp;
+/// <summary>
+/// Interaction logic for App.xaml
+/// </summary>
+public partial class App : Application
+{
+}
+
---
+++ src/ResponsiveApp/MainWindow.xaml.cs
@@ -0,0 +1,11 @@
+﻿using System.Windows;
+
+namespace ResponsiveApp;
+
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+}
