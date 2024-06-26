b7b2213bd70309e49b6c2841459ae5def473f540
Pohan <oange6214@gmail.com>
2024-06-26 09:15:52 +0800

    Edit Dashboard.Modern 1. Add assets (include image, font, style) 2. Create model. 3. Create Command class.

---
+++ src/Dashborad.Modern/Inputs/RelayCommand.cs
@@ -0,0 +1,26 @@
+﻿using System.Windows.Input;
+
+namespace Dashborad.Modern.Inputs;
+
+/// <summary>
+/// RelayCommand allows you to inject the command's logic via delegates passed into its contructor. This method
+/// enables ViewModel classes to implement commands in a concise manner.
+/// </summary>
+
+public class RelayCommand : ICommand
+{
+    private Action<object> _execute;
+    private Func<object, bool> _canExecute;
+
+    public event EventHandler? CanExecuteChanged;
+
+    public bool CanExecute(object? parameter)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void Execute(object? parameter)
+    {
+        throw new NotImplementedException();
+    }
+}
